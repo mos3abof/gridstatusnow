b8880536b41bb32a614a6d01a4d31c9be2da3b16
Khaled Attia <khaled.attia@googlemail.com>
2014-08-07 13:57:54 +0300

    Added PowerGridService to make the logic reusable

--- src/PowerGrid/PowerGridBundle/Controller/DefaultController.php
+++ src/PowerGrid/PowerGridBundle/Controller/DefaultController.php
@@ -2,85 +2,23 @@
 
 namespace PowerGrid\PowerGridBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+    Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
 	public function indexAction()
 	{
-		$status_array = array(
-			'status' => 'Unknown',
-			'cssclass' => 'danger',
-		);
-
-		try
-		{
-
-			$content = @file_get_contents('http://loadmeter.egyptera.org/MiniCurrentLoadClock3.aspx');
-			$content = strtolower($content);
-            if (strcmp(trim($content),'') == 0)
-            {
-                throw new Exception("Source seems to be down", 1);
-                
-            }
-
-			$power_grid_status = array('status' => 'Unknown Status');
-
-			if(strpos($content, 'images/c3.gif') !== false)
-			{
-				$status_array = array(
-					'status' => 'Danger Zone',
-					'cssclass' => 'danger',
-				);
-			}
-			elseif(strpos($content, 'images/c2.gif') !== false)
-			{
-				$status_array = array(
-					'status' => 'Warning Zone',
-					'cssclass' => 'warning',
-				);
-			}
-			elseif(strpos($content, 'images/c1.gif') !== false)
-			{
-				$status_array = array(
-					'status' => 'Safe Zone',
-					'cssclass' => 'success',
-				);
-			}
-		}
-		catch(Exception $e)
-		{
-			$status_array = array(
-				'status' => 'Unknown Status. Source seems to be down!',
-				'cssclass' => 'danger',
-			);
-		}
-
-		return $this->render('PowerGridBundle:Default:index.html.twig', array('status_array' => $status_array));
+        return $this->render('PowerGridBundle:Default:index.html.twig', [
+            'status' => $this->get('power_grid_service')->getStatus()
+        ]);
 	}
 
 	public function statusAction()
 	{
-		$content = @file_get_contents('http://loadmeter.egyptera.org/MiniCurrentLoadClock3.aspx');
-		$content = strtolower($content);
-
-		$power_grid_status = array('status' => 'Unknown');
-
-		if(strpos($content, 'images/c3.gif') !== false)
-		{
-			$power_grid_status['status'] = 'Danger';
-		}
-		elseif(strpos($content, 'images/c2.gif') !== false)
-		{
-			$power_grid_status['status'] = 'Warning';
-		}
-		elseif(strpos($content, 'images/c1.gif') !== false)
-		{
-			$power_grid_status['status'] = 'Safe';
-		}
-
-        $status = json_encode($power_grid_status);
-		return $this->render('PowerGridBundle:Default:status.html.twig', array('status' => $status));
+        return new JsonResponse([
+            'status' => $this->get('power_grid_service')->getStatus()
+        ]);
 	}
 
 }
