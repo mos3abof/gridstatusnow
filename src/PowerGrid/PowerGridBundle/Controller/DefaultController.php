65e23d74c266670e790cc3a745d6fa2914db2acb
Mosab Ahmad <mahmad@yubb-software.com>
2014-08-20 00:08:51 +0300

    Added a simple history link

--- src/PowerGrid/PowerGridBundle/Controller/DefaultController.php
+++ src/PowerGrid/PowerGridBundle/Controller/DefaultController.php
@@ -9,16 +9,22 @@ class DefaultController extends Controller
 {
 	public function indexAction()
 	{
-        return $this->render('PowerGridBundle:Default:index.html.twig', [
+        return $this->render('PowerGridBundle:Default:index.html.twig', array(
             'status' => $this->get('power_grid_service')->getStatus()
-        ]);
+        ));
 	}
 
 	public function statusAction()
 	{
-        return new JsonResponse([
+        return new JsonResponse(array(
             'status' => $this->get('power_grid_service')->getStatus()
-        ]);
+        ));
 	}
+    
+    
+    public function historyAction()
+    {
+        return $this->render('PowerGridBundle:Default:history.html.twig');
+    }
 
 }
