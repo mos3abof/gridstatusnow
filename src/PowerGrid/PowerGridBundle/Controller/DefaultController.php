5512cee2cc000b8ed1b22900f764c4d04cbf6d09
Mosab Ahmad <mahmad@yubb-software.com>
2015-01-11 19:46:02 +0200

    Today now works dynamically without the need to generate tsv files

--- src/PowerGrid/PowerGridBundle/Controller/DefaultController.php
+++ src/PowerGrid/PowerGridBundle/Controller/DefaultController.php
@@ -19,7 +19,14 @@ class DefaultController extends Controller
 
 		$current_date = $date->format('l, F jS');
 
-		return $this->render('PowerGridBundle:Default:index.html.twig', array('status' => $this->get('power_grid_service')->getStatus(), 'today' => $current_date));
+		$template_vars = array(
+			'status' => $this->get('power_grid_service')->getStatus(),
+			'today' => $current_date,
+			'day_number' => $date->format('d'),
+			'month_number' => $date->format('m'),
+			'year_number' => $date->format('Y'),
+		);
+		return $this->render('PowerGridBundle:Default:index.html.twig', $template_vars);
 	}
 
 	public function statusAction()
@@ -82,22 +89,139 @@ class DefaultController extends Controller
 		return $this->render('PowerGridBundle:Default:history.html.twig', array('month' => $month, 'd3_days' => $d3_days));
 	}
 
-	public function ApiAction()
+
+	public function historytsvAction($year, $month, $day = '')
+	{
+		$repository = $this->getDoctrine()->getRepository('PowerGridBundle:Status');
+
+		// global values and configs
+		$plus = $this->container->getParameter('gmt_as_number');
+		$web_dir_path = $this->container->getParameter('web_dir_path');
+		$tz = $this->container->getParameter('default_timezone');;
+
+		$status_to_number = array(
+			'Safe'    => 3,
+			'Warning' => 2,
+			'Danger'  => 1,
+			'Unknown' => 1
+		);
+
+		if(isset($day) && $day != '')
+		{
+			// Querying data for day
+			$day_date = new \DateTime($year . '-' . $month . '-' . $day);
+			$first_delimiter = $day_date->setTimezone(new \DateTimeZone($tz))->modify('+' . $plus . ' hour');
+			$last_delimiter = $day_date->setTimezone(new \DateTimeZone($tz))->modify('+1 day')->modify('+' . $plus . ' hour');
+
+			$day_loop = true;
+
+		}
+		else
+		{
+			// Querying data for month
+			$first_delimiter = new \DateTime($year . '-' . $month . '-01');
+			$first_delimiter->modify('first day of this month');
+
+			$last_delimiter =  new \DateTime($year . '-' . $month . '-01');
+			$last_delimiter->modify('last day of this month');
+		}
+
+		// Create a query
+		$query = $repository->createQueryBuilder('p')
+			->where('p.timestamp BETWEEN :starting AND :ending')
+			->setParameter('starting', $first_delimiter->format('Y-m-d 00:00:00'))
+			->setParameter('ending', $last_delimiter->format('Y-m-d 23:59:59'))
+			->orderBy('p.id', 'ASC')
+			->getQuery();
+
+		// Get the query result
+		$records = $query->getResult();
+
+		// Initialize an array to process results
+		$averaged_load_result = array();
+
+		// If we got any results process them
+		if(!$records)
+		{
+			print 'No results exist!';
+			exit;
+		}
+
+		$tsv_output = "day\thour\tvalue\n";
+
+		foreach($records as $key => $object)
+		{
+			$day_number = intval($object->getTimestamp()->format('d'));
+			$hour_number = intval($object->getTimestamp()->format('H'));
+			$status_number = $status_to_number[$object->getStatus()];
+
+			$averaged_load_result[$day_number][$hour_number][1] = 0;
+			$averaged_load_result[$day_number][$hour_number][2] = 0;
+			$averaged_load_result[$day_number][$hour_number][3] = 0;
+
+			$averaged_load_result[$day_number][$hour_number][$status_number]++;
+		}
+
+
+		// Prepare records for one day
+		if(isset($day_loop) && $day_loop == true)
+		{
+			for($hour = 0; $hour <= count($averaged_load_result[$day_number]); $hour++)
+			{
+				if(isset($averaged_load_result[$day_number][$hour]))
+				{
+					$hour_value = array_search(max($averaged_load_result[$day_number][$hour]), $averaged_load_result[$day_number][$hour]);
+
+					$tsv_output .= "1\t" . ($hour + 1) . "\t" . $hour_value . "\n";
+				}
+			}
+		}
+		// prepare records for a nonth
+		else
+		{
+			for($day_record = 1; $day_record <= count($averaged_load_result); $day_record++)
+			{
+				if(isset($averaged_load_result[$day_record]))
+				{
+					for($hour = 0; $hour <= count($averaged_load_result[$day_record]); $hour++)
+					{
+						if(isset($averaged_load_result[$day_record][$hour]))
+						{
+							$hour_value = array_search(max($averaged_load_result[$day_record][$hour]), $averaged_load_result[$day_record][$hour]);
+							$tsv_output .= $day_record . "\t" . ($hour + 1) . "\t" . $hour_value . "\n";
+						}
+					}
+				}
+			}
+		}
+
+
+		$tsv_output .= "34\t10\t3\n";
+		$tsv_output .= "34\t11\t2\n";
+		$tsv_output .= "34\t12\t1\n";
+
+		print $tsv_output;
+		exit;
+
+
+	}
+
+	public function apiAction()
 	{
 		return $this->render('PowerGridBundle:Default:api.html.twig');
 	}
 
-	public function AboutAction()
+	public function aboutAction()
 	{
 		return $this->render('PowerGridBundle:Default:about.html.twig');
 	}
 
-	public function FaqAction()
+	public function faqAction()
 	{
 		return $this->render('PowerGridBundle:Default:faq.html.twig');
 	}
 
-	public function ContactAction()
+	public function contactAction()
 	{
 		return $this->render('PowerGridBundle:Default:contact.html.twig');
 	}
